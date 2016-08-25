4eeae7ad059d6202a25d498ce2b32a9a1c1b93c2
previtus <previtus@gmail.com>
2016-08-25 20:22:49 +0530

    Opened as list already

--- GraphParser/CSVParser.py
+++ GraphParser/CSVParser.py
@@ -1,28 +1,15 @@
 import csv
 import time
 from Loaders import *
-import pandas
 start_time = time.time()
 
-
-edges_reader = open_with_python_csv_list(r'../../graph/edges_.csv')
-nodes_reader = open_with_python_csv_list(r'../../graph/nodes_.csv')
-
-# parse to have list Edges
-# with indexes to node in Nodes
-
-Edges = []
-Nodes = []
-
-for edge in edges_reader:
-    #print edge
-    Edges.append(edge)
-
-for node in nodes_reader:
-    #print node
-    Nodes.append(node)
+Edges = open_with_python_csv_list(r'../../graph/edges_.csv')
+Nodes = open_with_python_csv_list(r'../../graph/nodes_.csv')
 
 print "loaded edges: ", len(Edges)
+print Edges[:1]
 print "loaded nodes: ", len(Nodes)
+print Nodes[:1]
 
 print("--- %s seconds ---" % round(time.time() - start_time, 2))
+
