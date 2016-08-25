d59397a9bc85d1ec70dbc5b20393a6b6fab8f32a
previtus <previtus@gmail.com>
2016-08-25 15:48:53 +0530

    csv naive parsing, get img from url
    - csv should be loaded wholy first and then the segments should be taken
    from it
    - imgs should have correct heading and be named in some way

---
+++ 01b_csv_parses.py
@@ -0,0 +1,32 @@
+import csv
+from fce01c_csv_load_segment_by_ids import loadSegment
+
+csv_file = open(r'edges_small.csv')
+reader = csv.reader(csv_file, skipinitialspace=True)
+header = reader.next()
+print header
+
+random_segment = reader.next() # first line
+print random_segment
+
+FromId = random_segment[0]
+ToId = random_segment[1]
+Popularity = random_segment[4]
+
+Segment = loadSegment(FromId, ToId)
+print "from"
+print Segment[0][0]
+print Segment[0][2]
+print "to"
+print Segment[1][0]
+print Segment[1][2]
+
+url1 = ["http://maps.googleapis.com/maps/api/streetview?size=600x400&location=",Segment[0][0],",",Segment[0][2],"&heading=151.78&key="]
+url2 = ["http://maps.googleapis.com/maps/api/streetview?size=600x400&location=",Segment[1][0],",",Segment[1][2],"&heading=151.78&key="]
+
+print "".join(url1)
+print "".join(url2)
+
+import urllib
+urllib.urlretrieve("".join(url1), "A.jpg")
+urllib.urlretrieve("".join(url2), "B.jpg")
