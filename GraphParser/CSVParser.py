37579a3e1c1a362c58fa1cf0da0c44beffb98bd8
previtus <previtus@gmail.com>
2016-08-29 17:57:03 +0530

    Bearing - added to the Segment

--- GraphParser/CSVParser.py
+++ GraphParser/CSVParser.py
@@ -1,6 +1,9 @@
 import csv
 import time
 from Loaders import *
+from Functions import *
+
+
 start_time = time.time()
 
 Edges = open_with_python_csv_list(r'../../graph/edges_first5.csv')
@@ -24,8 +27,18 @@ class GraphEdgeSegment:
     def __init__(self, FromId, ToId, Nodes):
         self.FromId = FromId
         self.ToId = ToId
-        self.Start = Nodes[FromId]
-        self.End = Nodes[ToId]
+
+        # Latitude,Latitude projected,Longitude,Longitude projected,Elevation
+        #self.Start = map(float, Nodes[FromId])
+        #self.End = map(float, Nodes[ToId])
+        self.Start = ( float(Nodes[FromId][0]), float(Nodes[FromId][2]) )
+        self.End = ( float(Nodes[ToId][0]), float(Nodes[ToId][2]) )
+
+        self.ProjectedStart = ( float(Nodes[FromId][1]), float(Nodes[FromId][3]) )
+        self.ProjectedEnd = ( float(Nodes[ToId][1]), float(Nodes[ToId][3]) )
+        
+        self.ElevationStart = float(Nodes[FromId][4])
+        self.ElevationEnd = float(Nodes[ToId][4])
         
         GraphEdgeSegment.tmp += 1
    
@@ -34,11 +47,8 @@ class GraphEdgeSegment:
         print "Start: ", self.Start
         print "End: ", self.End
 
-    def getLocationStringStart(self):
-        return "".join( [self.Start[0],",", self.Start[2]] )
-    def getLocationStringEnd(self):
-        return "".join( [self.End[0],",", self.End[2]] )
-
+    def getBearingString(self):
+        return bearing_between_two_points(self.Start, self.End)
 
 
 for edge in Edges[:NumEdgesToProcess]:
@@ -48,6 +58,6 @@ for edge in Edges[:NumEdgesToProcess]:
 
     segment = GraphEdgeSegment(FromId, ToId, Nodes)
     segment.displaySegment()
-    print segment.getLocationStringStart()
+    print segment.getBearingString()
 
     print ''#edge
---
+++ GraphParser/Functions.py
@@ -0,0 +1,23 @@
+from math import *
+
+def bearing_between_two_points(start, end):
+    '''
+    Calculates the initial bearing between two geographical locations.
+    see: http://www.movable-type.co.uk/scripts/latlong.html
+    The bearing is angular distance from NORTH.
+    '''
+    if (type(start) != tuple) or (type(end) != tuple):
+        print type(start)
+        raise TypeError("Function bearing_between_two_points takes only tuples.")
+    
+    lat1 = radians(start[0])
+    lat2 = radians(end[0])
+    dLong = radians(end[1] - start[1])
+    
+    x = sin(dLong) * cos(lat2)
+    y = cos(lat1) * sin(lat2) - ( sin(lat1) * cos(lat2) * cos(dLong) )
+    
+    bearing = degrees( atan2(x,y) )
+    bearing_from_north = (bearing + 360) % 360
+
+    return bearing_from_north
