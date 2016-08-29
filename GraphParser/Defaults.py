a21e87cafd2658780b2a4dda928574713fca20be
previtus <previtus@gmail.com>
2016-08-29 23:32:20 +0530

    Download with Error handling and Retry macro
    Retries downloading the file upon Exception - usually on TimeOut.
    TimeOut on purpose set to low to prevent "lags" when the pc seems to be
    waiting for Godot and not really progressing anywhere. Hard value in
    Defaults.py, might need to be adjusted.

--- GraphParser/Defaults.py
+++ GraphParser/Defaults.py
@@ -1,7 +1,9 @@
 # SEGMENTS TO BE LOADED
 
 FromEdgeID = 0 # 2 -> will start with 002.jpg
-ToEdgeID = 5 # 14 -> will end with 013.jpg
+ToEdgeID = 100 # 14 -> will end with 013.jpg
 PIXELS_X = 300
 PIXELS_Y = 150
 NUMBER_OF_ZEROS_PADDING = '03'
+
+DOWNLOAD_TIMEOUT = 0.7
--- GraphParser/DownloadUrlFilenameMap.py
+++ GraphParser/DownloadUrlFilenameMap.py
@@ -1,4 +1,11 @@
+import socket
 import urllib
+from DecoratorRetry import retry
+from Defaults import *
+
+@retry(Exception, tries=4, delay=3, backoff=2)
+def urlretrieve_with_retry(url, filename):
+    return urllib.urlretrieve(url, filename)
 
 def DownloadUrlFilenameMap(FilenameMap):
     '''
@@ -6,15 +13,24 @@ def DownloadUrlFilenameMap(FilenameMap):
     List of tuples in [ (<url>, <filename>), ... ]
     '''
 
+    # timeout in seconds
+    socket.setdefaulttimeout(DOWNLOAD_TIMEOUT)
+
+
     for fileTuple in FilenameMap:
         url = fileTuple[0]
         filename = fileTuple[1]
 
         print filename
 
-        # TODO: something more reliable than urlretrieve - with error handling, reconnections after some time etc.
-        urllib.urlretrieve(url, filename)
+        urlretrieve_with_retry(url, filename)
     
     return []
 
 
+# Exception example:
+'''
+images/078.jpg
+Exception:  [Errno socket error] [Errno 11001] getaddrinfo failed
+Exception:  [Errno socket error] timed out
+'''
