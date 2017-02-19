93311ad50a4340a70c3a59d0b225a8c647c77abf
previtus <previtus@gmail.com>
2017-02-19 14:06:53 +0100

    Multiple histories visualization.

--- GraphParser/VisualizeHistory.py
+++ GraphParser/VisualizeHistory.py
@@ -27,6 +27,38 @@ def visualize_history(hi, show=True, save=False, save_path=''):
 
     return plt
 
+def visualize_histories(histories, names, show=True, save=False, save_path=''):
+    '''
+    Visualize multiple histories.
+
+    Example usage:
+        h1 = loadHistory('history1.npy')
+        h2 = loadHistory('history2.npy')
+        visualize_histories([h1, h2], ['history1', 'history2'])
+    '''
+    i = 0
+    leg = []
+    for hi in histories:
+        n = names[i]
+        # list all data in history
+        print(hi.keys())
+        # summarize history for loss
+        plt.plot(hi['loss'])
+        plt.plot(hi['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        leg.append(n + '_train')
+        leg.append(n + '_test')
+        i += 1
+    plt.legend(leg, loc='upper left')
+    if save:
+        plt.savefig(save_path+'loss.png')
+    if show:
+        plt.show()
+
+    return plt
+
 def saveHistory(history_dict, filename):
     to_be_saved = data = {'S': history_dict}
     np.save(open(filename, 'w'), to_be_saved)
@@ -36,3 +68,8 @@ def loadHistory(filename):
     return loaded[()]['S']
 
 #visualize_history(loadHistory('1100downloaded_vII/tmp_saved_history.npy'))
+
+#h1 = loadHistory('/home/ekmek/TEMP_SPACE/MGR-Project-Code/GraphParser/ModelImplementations/1_SimpleCNN_3conv_2fc/results/history.npy')
+#h2 = loadHistory('/home/ekmek/TEMP_SPACE/MGR-Project-Code/GraphParser/ModelImplementations/2_VGG16_no_finetuning/(runs)/now2/results/history.npy')
+#visualize_histories([h1, h2], ['simple', 'vgg'])
+
