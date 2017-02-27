bcae0493e4cc59d17b51a5ac67ee71d5e994aca7
previtus <previtus@gmail.com>
2017-02-27 11:21:28 +0100

    Custom datasets (current full size is 8376, we are getting more resizes etc.)

--- DatasetHandler/CreateDataset.py
+++ DatasetHandler/CreateDataset.py
@@ -1,31 +1,61 @@
 from DatasetObj import Dataset
 import random
 
-def load_subset_8376_valid_images_640x640_120deg_turns_from_all_segments(desired_number, seed=42):
+ABS_PATH_TO_PRJ = '/home/ekmek/Project II/MGR-Project-Code/'
+
+def load_8376_valid_images_640x640_120deg_turns_from_all_segments(desired_number=None, seed=None):
     '''
     :param desired_number: size of generated subset (for example 1000 out of the 8376)
     :param seed: random seed to allow for equal results every time
     :return:
     '''
-    path = '../Data/StreetViewData/8376_valid_images_640x640_120deg_turns_from_all_segments/SegmentsData.dump'
+    path = ABS_PATH_TO_PRJ+'Data/StreetViewData/8376_valid_images_640x640_120deg_turns_from_all_segments/SegmentsData.dump'
     dataset = Dataset()
     dataset.init_from_segments(path, 640, 640)
+    if desired_number is None:
+        return dataset
 
     if seed is not None:
         random.seed(seed)
 
     subset = dataset.spawnUniformSubset(desired_number)
-
     return subset
 
-def load_8376_valid_images_640x640_120deg_turns_from_all_segments():
-    path = '../Data/StreetViewData/8376_valid_images_640x640_120deg_turns_from_all_segments/SegmentsData.dump'
-    dataset = Dataset()
-    dataset.init_from_segments(path, 640, 640)
-    return dataset
-
-def load_3342_calid_images_299x299():
-    path = '../Data/StreetViewData/3342_calid_images_299x299/SegmentsData.dump'
+def load_8376_resized_299x299(desired_number=None, seed=None):
+    path = ABS_PATH_TO_PRJ+'Data/StreetViewData/8376_valid_images_resized_299x299/SegmentsData.dump'
     dataset = Dataset()
     dataset.init_from_segments(path, 299, 299)
-    return dataset
+    if desired_number is None:
+        return dataset
+    if seed is not None:
+        random.seed(seed)
+    subset = dataset.spawnUniformSubset(desired_number)
+    return subset
+
+def load_8376_resized_150x150(desired_number=None, seed=None):
+    path = ABS_PATH_TO_PRJ+'Data/StreetViewData/8376_valid_images_resized_150x150/SegmentsData.dump'
+    dataset = Dataset()
+    dataset.init_from_segments(path, 150, 150)
+    if desired_number is None:
+        return dataset
+    if seed is not None:
+        random.seed(seed)
+    subset = dataset.spawnUniformSubset(desired_number)
+    return subset
+
+def load_3342_valid_images_299x299(desired_number=None, seed=None):
+    path = ABS_PATH_TO_PRJ+'Data/StreetViewData/3342_calid_images_299x299/SegmentsData.dump'
+    dataset = Dataset()
+    dataset.init_from_segments(path, 299, 299)
+    if desired_number is None:
+        return dataset
+
+    if seed is not None:
+        random.seed(seed)
+
+    subset = dataset.spawnUniformSubset(desired_number)
+    return subset
+
+#d = load_8376_resized_299x299()
+#d.statistics()
+#d.plotHistogram()
