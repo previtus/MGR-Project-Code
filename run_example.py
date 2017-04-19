84741b8f6c7aec220b8644d92535e1a805c65b08
previtus <previtus@gmail.com>
2017-04-19 19:14:54 +0900

    Add the support for custom setting given by bash script (run as "python_script.py sizeOfDataset numOfEpochs nameOfExp")

--- run_example.py
+++ run_example.py
@@ -2,6 +2,23 @@ import keras
 from DatasetHandler.CreateDataset import *
 from ModelHandler.CreateModel.functions_for_vgg16 import *
 import time
+import sys
+
+# DEFAULT VALUES:
+TMP_size_of_dataset=100
+TMP_num_of_epochs=150
+name_of_the_experiment = '-newWawe-1stRoundShouldCountBoth'
+
+# python python_script.py var1 var2 var3
+if len(sys.argv) > 3:
+    TMP_size_of_dataset = int(sys.argv[1])  # var1
+    TMP_num_of_epochs = int(sys.argv[2])  # var2
+    name_of_the_experiment = sys.argv[3]  # var3
+else:
+    print "Using default values. Please run as: python_script.py sizeOfDataset numOfEpochs nameOfExp"
+
+print "[Setting] python_script.py", TMP_size_of_dataset, TMP_num_of_epochs, name_of_the_experiment
+
 
 #d = load_8376_resized_150x150(desired_number=10)
 #d.statistics()
