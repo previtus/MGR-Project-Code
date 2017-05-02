3bccd314e69f319d811a74e8444c759d9166f63b
previtus <previtus@gmail.com>
2017-05-02 17:42:43 +0900

    added to the last one

--- DatasetHandler/FileHelperFunc.py
+++ DatasetHandler/FileHelperFunc.py
@@ -9,6 +9,19 @@ def get_project_folder():
     ABS_PATH_TO_PRJ = use_path_which_exists(PATH_ALTERNATIVES)
     return ABS_PATH_TO_PRJ
 
+def get_geojson_path():
+    '''
+    Gives us the path directly to attractivity_previtus_data_1_edges.geojson
+    :return:
+    '''
+    folders = ['/home/ekmek/Desktop/Project II/graph_new_data/',
+                   '/home/ekmek/Vitek/graph_new_data/',
+                    '/storage/brno2/home/previtus/important_files/']
+    folder = use_path_which_exists(folders)
+
+    return folder+'attractivity_previtus_data_1_edges.geojson'
+
+
 def use_path_which_exists(list_of_possible_paths):
     used_path = ''
 
@@ -20,3 +33,7 @@ def use_path_which_exists(list_of_possible_paths):
         print "Error, cannot locate the path of project, will likely fail!"
 
     return used_path
+
+def make_folder_ifItDoesntExist(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
