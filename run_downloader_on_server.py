06db1bedac9ca0a27d0f53776b2a4ecd90cab312
previtus <previtus@gmail.com>
2017-05-02 18:53:28 +0900

    int oops

--- run_downloader_on_server.py
+++ run_downloader_on_server.py
@@ -8,10 +8,10 @@ pixels = 299
 
 # python python_script.py NAME FROMID TOID PIXELS
 if len(sys.argv) > 4:
-    name = int(sys.argv[1])  # var1
+    name = sys.argv[1]  # var1
     from_id = int(sys.argv[2])  # var2
-    to_id = sys.argv[3]  # var3
-    pixels = sys.argv[4]  # var4
+    to_id = int(sys.argv[3])  # var3
+    pixels = int(sys.argv[4])  # var4
 else:
     print "Using default values. Please run as: python_script.py nameOfFolder fromId toId pixels"
 
