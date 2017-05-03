ad4d8425dc33d52a05770259c6de39212b5db620
previtus <previtus@gmail.com>
2017-05-03 13:56:48 +0900

    Mark new dataset

--- OSMHandler/TestDebugs.py
+++ OSMHandler/TestDebugs.py
@@ -18,7 +18,7 @@ def testCheckingSegments():
 
     #path_to_segments_file = '/home/ekmek/Desktop/Project II/MGR-Project-Code/Data/StreetViewData/50_rewritingObj_299x299/SegmentsData.dump'
     #path_to_segments_file = '/home/ekmek/Desktop/Project II/MGR-Project-Code/Data/StreetViewData/50_rewritingObj_299x299/SegmentsData_marked.dump'
-    path_to_segments_file = '/home/ekmek/Vitek/MGR-Project-Code/Data/StreetViewData/tmp_one/SegmentsData.dump'
+    path_to_segments_file = '/home/ekmek/Vitek/MGR-Project-Code/Data/StreetViewData/1200x_markable_640x640/SegmentsData.dump'
     Segments = Downloader.DataOperations.LoadDataFile(path_to_segments_file)
 
     #Segments = Segments[0:3]
@@ -34,7 +34,7 @@ def testCheckingSegments():
     allright = Checker.Check(Segments)
     print "checks out as ", allright
 
-    path2 = '/home/ekmek/Vitek/MGR-Project-Code/Data/StreetViewData/tmp_one/SegmentsData_marked.dump'
+    path2 = '/home/ekmek/Vitek/MGR-Project-Code/Data/StreetViewData/1200x_markable_640x640/SegmentsData_marked_R100.dump'
     Segments = Downloader.DataOperations.SaveDataFile(path2, Segments)
 
 
