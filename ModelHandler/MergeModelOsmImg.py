1cba43b17089bf7f4780ee9d8e99cc6a2700ad1e
previtus <previtus@gmail.com>
2017-05-03 22:07:50 +0900

    Model with both OSM and IMGS as inputs - concatenate

--- ModelHandler/MergeModelOsmImg.py
+++ ModelHandler/MergeModelOsmImg.py
@@ -2,7 +2,6 @@ import DatasetHandler.CreateDataset as CreateDataset
 from ModelHandler.CreateModel.ModelsFunctions import load_features, build_top_model, train_top_model, save_model_history
 from Downloader.ImageHelpers import len_
 
-
 # image features file
 img_features_train_file = '/home/ekmek/Vitek/Logs/shared/features_train_data299mark-full-seed42_resnet50.npy'
 img_features_val_file = '/home/ekmek/Vitek/Logs/shared/features_validation_data299mark-full-seed42_resnet50.npy'
@@ -17,10 +16,9 @@ print 'x', len_(x), 'y', len_(y), 'osm', len_(osm)
 
 print "input shape of features", len_(train_data), "and labels", len_(train_labels)
 
+# -------------------------------------------------------------------------------------------
 
-
-from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense, BatchNormalization
+from keras.layers import Dropout, Flatten
 from keras.utils import plot_model
 import numpy as np
 from Downloader.VisualizeHistory import visualize_history
@@ -28,35 +26,36 @@ from Downloader.VisualizeHistory import visualize_history
 osm = np.asarray(osm)
 osm_val = np.asarray(osm_val)
 
-input_shape = osm.shape[1:]
-print input_shape
+input_shape_osm = osm.shape[1:]
+input_shape_img = train_data.shape[1:]
+print input_shape_osm, input_shape_img
 
 number_of_repeats = 3
-osm_features = Sequential()
-osm_features.add(Dense(256, activation='relu',input_shape=input_shape))
-osm_features.add(Dropout(0.5))
 
-for i in range(0,number_of_repeats-1):
-    osm_features.add(Dense(256, activation='relu'))
-    osm_features.add(Dropout(0.5))
-osm_features.add(Dense(1, activation='sigmoid'))
-osm_features.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['mean_absolute_error'])
+from keras.models import Sequential, Model
+from keras.layers import Concatenate, Dense, LSTM, Input, concatenate
+osm_features_input = Input(shape=input_shape_osm)
+osm_features = Dense(256, activation='relu')(osm_features_input)
+osm_features = Dropout(0.5)(osm_features)
 
-plot_model(osm_features, to_file='-1---to-score.png', show_shapes=True)
+img_features_input = Input(shape=input_shape_img)
+img_features = Flatten()(img_features_input)
 
-img_features = build_top_model(train_data.shape[1:], 3)
-img_features.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['mean_absolute_error'])
+top = concatenate([osm_features, img_features])
+for i in range(0,number_of_repeats):
+    top = Dense(256, activation='relu')(top)
+    top = Dropout(0.5)(top)
+top = Dense(1, activation='sigmoid')(top)
 
-plot_model(img_features, to_file='-2---to-score.png', show_shapes=True)
+model = Model(inputs=[osm_features_input, img_features_input], outputs=top)
+model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['mean_absolute_error'])
+plot_model(model, to_file='v1-250.png', show_shapes=True)
 
-
-
-epochs = 100
-history = osm_features.fit(osm, train_labels,
+epochs = 250
+history = model.fit([osm, train_data], train_labels,
               epochs=epochs, batch_size=32,
-              validation_data=(osm_val, validation_labels))
+              validation_data=([osm_val, validation_data], validation_labels))
 history = history.history
 
-name = '/home/ekmek/Vitek/MGR-Project-Code/ModelHandler/'+str(number_of_repeats)+"repeats"+str(epochs)+"epochs---waaaa"
+name = '/home/ekmek/Vitek/MGR-Project-Code/ModelHandler/concat-'+str(number_of_repeats)+"repeats"+str(epochs)+"epochs---v1-250"
 visualize_history(history, show=True, save=True, save_path=name)
-
