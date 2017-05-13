8b371b94776616ca85317de59499b848f5954313
previtus <previtus@gmail.com>
2017-05-13 20:02:31 +0900

    Setting file for models_versus tuned for server run.

--- Settings/models_versus.py
+++ Settings/models_versus.py
@@ -1,11 +1,12 @@
 def Setup(Settings,DefaultModel):
     Settings["experiment_name"] = "Comparison_of_models"
 
-    Settings["graph_histories"] = ['together'] #['all','together',[],[1,0],[0,0,0],[]]
+    Settings["graph_histories"] = ['together', [0,1], [1,2], [0,2]] #['all','together',[],[1,0],[0,0,0],[]]
 
     Settings["models"][0]["model_type"] = 'img_osm_mix'
     Settings["models"][0]["unique_id"] = 'mix'
     Settings["models"][0]["top_repeat_FC_block"] = 2
+    Settings["models"][0]["epochs"] = 300
 
     Settings["models"].append(DefaultModel.copy())
 
@@ -13,12 +14,14 @@ def Setup(Settings,DefaultModel):
     Settings["models"][1]["model_type"] = 'osm_only'
     Settings["models"][1]["unique_id"] = 'osm_only'
     Settings["models"][1]["top_repeat_FC_block"] = 2
+    Settings["models"][0]["epochs"] = 300
 
     Settings["models"].append(DefaultModel.copy())
 
     Settings["models"][2]["dataset_pointer"] = 0 # 0 - reuse the first dataset
-    Settings["models"][2]["model_type"] = 'img_osm_mix'
+    Settings["models"][2]["model_type"] = 'simple_cnn_with_top'
     Settings["models"][2]["unique_id"] = 'img_only'
     Settings["models"][2]["top_repeat_FC_block"] = 2
+    Settings["models"][0]["epochs"] = 300
 
     return Settings
