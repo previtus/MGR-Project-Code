2ae13c9cc2cd41a3f720e55139ebef8228617301
previtus <previtus@gmail.com>
2017-05-13 15:12:24 +0900

    Added try + except blocks

--- Omnipresent.py
+++ Omnipresent.py
@@ -5,64 +5,82 @@ def len_(L):
     return np.array(L).shape
 
 def send_main(subject, message, attachment_path = None):
+    try:
+        import smtplib, os
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+        from email.mime.base import MIMEBase
+        from email import encoders
 
-    import smtplib, os
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    from email.mime.base import MIMEBase
-    from email import encoders
+        from mail_secrets import fromaddr, toaddr, passwrd
+        print "Sending report mail to", toaddr
 
-    from mail_secrets import fromaddr, toaddr, passwrd
+        msg = MIMEMultipart()
 
-    msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = subject #"SUBJECT OF THE EMAIL"
 
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = subject #"SUBJECT OF THE EMAIL"
+        body = message #"TEXT YOU WANT TO SEND"
 
-    body = message #"TEXT YOU WANT TO SEND"
+        msg.attach(MIMEText(body, 'plain'))
 
-    msg.attach(MIMEText(body, 'plain'))
+        if attachment_path is not None:
 
-    if attachment_path is not None:
+            attachment_filename = os.path.basename(attachment_path)
+            attachment = open(attachment_path, "rb") #"PATH OF THE FILE"
 
-        attachment_filename = os.path.basename(attachment_path)
-        attachment = open(attachment_path, "rb") #"PATH OF THE FILE"
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload((attachment).read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', "attachment; filename= %s" % attachment_filename)
 
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= %s" % attachment_filename)
+            msg.attach(part)
 
-        msg.attach(part)
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, passwrd)
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, passwrd)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+        return True
+    except Exception as inst:
+        print "Exception when trying to send mail."
+        print type(inst)     # the exception instance
+        print inst.args      # arguments stored in .args
+        print inst           # __str__ allows args to be printed directly
+        return False
 
 
 #send_main('automatic mail', 'test', '/home/ekmek/Vitek/Logs/Number_of_FC_blocks_test/graph_together_Number_of_FC_blocks_test.png')
 
 def save_job_report_page(folder_path, job_id, cut = True):
-    url = 'https://metavo.metacentrum.cz/pbsmon2/job/' + job_id
+    try:
+        print "Downloading ", job_id+'.html'
 
-    response = urllib2.urlopen(url)
-    webContent = response.read()
+        url = 'https://metavo.metacentrum.cz/pbsmon2/job/' + job_id
 
-    if cut:
-        substr = 'Job '+job_id
-        index = webContent.replace(substr,'ignore', 1).find(substr)
-        offset = 18
-        webContent = webContent[index+offset:-1]
+        response = urllib2.urlopen(url)
+        webContent = response.read()
 
-    #print(webContent)
+        if cut:
+            substr = 'Job '+job_id
+            index = webContent.replace(substr,'ignore', 1).find(substr)
+            offset = 18
+            webContent = webContent[index+offset:-1]
 
-    f = open(folder_path + job_id+'.html', 'w')
-    f.write(webContent)
-    f.close()
+        #print(webContent)
+
+        f = open(folder_path + job_id+'.html', 'w')
+        f.write(webContent)
+        f.close()
+        return True
+    except Exception as inst:
+        print "Exception in the report page downloading from Metacentrum"
+        print type(inst)     # the exception instance
+        print inst.args      # arguments stored in .args
+        print inst           # __str__ allows args to be printed directly
+        return False
 
 
 #save_job_report_page(folder_path='',job_id='1398409.arien-pro.ics.muni.cz')
