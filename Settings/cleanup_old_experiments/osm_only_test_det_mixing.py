17918485abd5653ea18b2bc950c13f4ba1db3492
previtus <previtus@gmail.com>
2017-05-24 20:45:11 +0900

    Cleanup

--- Settings/cleanup_old_experiments/osm_only_test_det_mixing.py
+++ Settings/cleanup_old_experiments/osm_only_test_det_mixing.py
@@ -11,19 +11,19 @@ def Setup(Settings,DefaultModel):
     Settings["models"][n]["model_type"] = 'osm_only'
     Settings["models"][n]["unique_id"] = 'same_segment'
     Settings["models"][n]["top_repeat_FC_block"] = 2
-    Settings["models"][n]["epochs"] = 50
+    Settings["models"][n]["epochs"] = 100
 
     Settings["models"][n]["special_case"] == 'same_segment'
 
 
     Settings["models"].append(DefaultModel.copy())
     n+=1
-    Settings["models"][n]["dataset_pointer"] = 0
+    Settings["models"][n]["dataset_pointer"] = -1
     Settings["models"][n]["dataset_name"] = "1200x_markable_299x299" # "1200x_markable_299x299", "5556x_mark_res_299x299", "5556x_markable_640x640"
     Settings["models"][n]["model_type"] = 'osm_only'
     Settings["models"][n]["unique_id"] = 'modulo'
     Settings["models"][n]["top_repeat_FC_block"] = 2
-    Settings["models"][n]["epochs"] = 50
+    Settings["models"][n]["epochs"] = 100
 
     Settings["models"][n]["special_case"] == 'modulo'
 
