f3f8ac7524a105d9a58737a7998d0554647d725a
previtus <previtus@gmail.com>
2017-05-25 16:17:03 +0900

    Preparation of settings file for run on server

--- Settings/models_versus_5556_640.py
+++ Settings/models_versus_5556_640.py
@@ -1,11 +1,11 @@
 def Setup(Settings,DefaultModel):
-    Settings["experiment_name"] = "Comparison_of_models_1200_299"
+    Settings["experiment_name"] = "Comparison_of_models_5556_640"
 
     Settings["graph_histories"] = ['together', [0,1], [1,2], [0,2]] #['all','together',[],[1,0],[0,0,0],[]]
     n=0
 
-    Settings["models"][n]["dataset_name"] = "1200x_markable_299x299" # "1200x_markable_299x299", "5556x_mark_res_299x299", "5556x_markable_640x640"
-    Settings["models"][n]["pixels"] = 299
+    Settings["models"][n]["dataset_name"] = "5556x_markable_640x640" # "1200x_markable_299x299", "5556x_mark_res_299x299", "5556x_markable_640x640"
+    Settings["models"][n]["pixels"] = 640
     Settings["models"][n]["model_type"] = 'img_osm_mix'
     Settings["models"][n]["unique_id"] = 'mix'
     Settings["models"][n]["top_repeat_FC_block"] = 2
