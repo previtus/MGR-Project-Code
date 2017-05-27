ab6e951a5bb1f85b7de6d1ba7b87ff44ecae14a2
previtus <previtus@gmail.com>
2017-05-27 19:30:17 +0900

    Major Cooking

--- Settings/basic_models_cooking.py
+++ Settings/basic_models_cooking.py
@@ -22,6 +22,28 @@ def Setup(Settings,DefaultModel):
 
     n=0
     Settings["models"][n]["model_type"] = 'simple_cnn_with_top'
+    Settings["models"][n]["dataset_name"] = "5556x_reslen30_299px"
+    Settings["models"][n]["pixels"] = 299
+    Settings["models"][n]["cnn_model"] = 'resnet50'
+    Settings["models"][n]["unique_id"] = 'resnet50_5556x_reslen30_299px'
+    Settings["models"][n]["cooking_method"] = 'generators' # 'direct' or 'generators'
+    Settings["models"][n]["epochs"] = 5
+
+    Settings["models"].append(DefaultModel.copy())
+    n=1
+    Settings["models"][n]["dataset_pointer"] = -1 # 0 - reuse the first dataset
+    Settings["models"][n]["model_type"] = 'simple_cnn_with_top'
+    Settings["models"][n]["dataset_name"] = "5556x_reslen20_299px"
+    Settings["models"][n]["pixels"] = 299
+    Settings["models"][n]["cnn_model"] = 'resnet50'
+    Settings["models"][n]["unique_id"] = 'resnet50_5556x_reslen20_299px'
+    Settings["models"][n]["cooking_method"] = 'generators' # 'direct' or 'generators'
+    Settings["models"][n]["epochs"] = 5
+
+    Settings["models"].append(DefaultModel.copy())
+    n=2
+    Settings["models"][n]["dataset_pointer"] = -1 # 0 - reuse the first dataset
+    Settings["models"][n]["model_type"] = 'simple_cnn_with_top'
     Settings["models"][n]["dataset_name"] = "5556x_minlen30_640px"
     Settings["models"][n]["pixels"] = 640
     Settings["models"][n]["cnn_model"] = 'resnet50'
@@ -29,18 +51,17 @@ def Setup(Settings,DefaultModel):
     Settings["models"][n]["cooking_method"] = 'generators' # 'direct' or 'generators'
     Settings["models"][n]["epochs"] = 5
 
-    '''
     Settings["models"].append(DefaultModel.copy())
-    n=1
+    n=3
     Settings["models"][n]["dataset_pointer"] = -1 # 0 - reuse the first dataset
     Settings["models"][n]["model_type"] = 'simple_cnn_with_top'
     Settings["models"][n]["dataset_name"] = "5556x_minlen20_640px"
     Settings["models"][n]["pixels"] = 640
     Settings["models"][n]["cnn_model"] = 'resnet50'
-    Settings["models"][n]["unique_id"] = 'resnet50_1200x_markable_299x299'
+    Settings["models"][n]["unique_id"] = 'resnet50_5556x_minlen20_640px'
     Settings["models"][n]["cooking_method"] = 'generators' # 'direct' or 'generators'
     Settings["models"][n]["epochs"] = 5
-    '''
+
 
     '''
     n=0
