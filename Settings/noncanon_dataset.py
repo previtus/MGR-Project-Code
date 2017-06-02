908d3c7ad3c766e6e127836f0759302be0183594
previtus <previtus@gmail.com>
2017-06-02 20:18:53 +0900

    Preparation of custom ImageDataGenerator

--- Settings/noncanon_dataset.py
+++ Settings/noncanon_dataset.py
@@ -20,7 +20,44 @@ def Setup(Settings,DefaultModel):
     n=0
     Settings["models"][n]["noncanon_dataset"] = 'expand_existing_dataset'
 
+    from keras.preprocessing.image import ImageDataGenerator
+    image_generator = ImageDataGenerator(
+            featurewise_center = False,  # set input mean to 0 over the dataset
+            samplewise_center = False,  # set each sample mean to 0
+            featurewise_std_normalization = False,  # divide inputs by std of the dataset
+            samplewise_std_normalization = False,  # divide each input by its std
+            zca_whitening = False,  # apply ZCA whitening
+            rotation_range = 0,  # randomly rotate images in the range (degrees, 0 to 180)
+            width_shift_range = 0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range = 0.1,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip = True,  # randomly flip images
+            vertical_flip = False,  # randomly flip images
+    )
 
+    defaults = ImageDataGenerator(
+        featurewise_center=False, # Set input mean to 0 over the dataset, feature-wise.
+        samplewise_center=False, # Set each sample mean to 0.
+        featurewise_std_normalization=False, # Divide inputs by std of the dataset, feature-wise.
+        samplewise_std_normalization=False, # Divide each input by its std.
+        zca_whitening=False, # Apply ZCA whitening.
+        rotation_range=0., # Degree range for random rotations.
+        width_shift_range=0., # Range for random horizontal shifts (fraction of total width).
+        height_shift_range=0., # Range for random vertical shifts (fraction of total height).
+        shear_range=0., # Shear Intensity (Shear angle in counter-clockwise direction as radians)
+        zoom_range=0., # Float or [lower, upper]. Range for random zoom. If a float, [lower, upper] = [1-zoom_range, 1+zoom_range].
+        channel_shift_range=0., # Range for random channel shifts.
+        fill_mode='nearest', # One of {"constant", "nearest", "reflect" or "wrap"}. Points outside the boundaries of the input are filled according to the given mode.
+        cval=0., # Float or Int. Value used for points outside the boundaries when fill_mode = "constant".
+        horizontal_flip=False, # Randomly flip inputs horizontally.
+        vertical_flip=False, # Randomly flip inputs vertically.
+        rescale=None, # rescaling factor. Defaults to None. If None or 0, no rescaling is applied, otherwise we multiply the data by the value provided (before applying any other transformation).
+        preprocessing_function=None,
+        # function that will be implied on each input. The function will run before any other modification on it.
+        # The function should take one argument: one image (Numpy tensor with rank 3), and should output a Numpy tensor with the same shape.
+    )
+
+    Settings["models"][n]["noncanon_dataset_imagegenerator"] = image_generator
+    Settings["models"][n]["noncanon_dataset_genfrom1"] = 1
 
     Settings["models"][n]["model_type"] = 'simple_cnn_with_top'
     Settings["models"][n]["dataset_name"] = "5556x_minlen30_640px"
