ab92bbaef88edb47dc7ba617314cf0d7be104108
previtus <previtus@gmail.com>
2017-06-17 23:33:02 +0900

    Functions prepared

--- ModelHandler/KfoldTester.py
+++ ModelHandler/KfoldTester.py
@@ -1,3 +1,27 @@
+import math
+import numpy
+
+def chunks(l, n):
+    a = numpy.array_split(numpy.array(l), n)
+    b = []
+    for i in a:
+        b.append(i.tolist())
+    return b
+
+def kfold(indices_in_fjords, selected):
+    # indices come like [] [] [] ... [], we want to select the one in <selected> as validation and rest as tests
+    i = 0
+    train_indices = []
+    validation_indices = indices_in_fjords[selected]
+    for fjord in indices_in_fjords:
+        if i<>selected:
+            train_indices += fjord
+        i += 1
+    return train_indices, validation_indices
+
+def indices_to_data(any_indices, any_data):
+    selected_data = [any_data[i] for i in any_indices]
+    return selected_data
 
 def k_fold_crossvalidation(model, dataset, model_settings):
     k = model_settings["crossvalidation_k"]
@@ -6,26 +30,35 @@ def k_fold_crossvalidation(model, dataset, model_settings):
     # with dataset having been shuffled already, we can just use the indices 0-number_of_images
 
     number_of_images_total = dataset.num_of_images
-
-    number_of_images_total = 97
+    number_of_images_total = 30
 
     print "Total of ", number_of_images_total, " images."
 
     indices = range(0, number_of_images_total)
 
-    last_index = 0
-    # indices in k folds
-    for i in range(0,k):
-        print "doing", i, "outta", k
+    indices_in_fjords = chunks(indices, k)
+    print indices_in_fjords
+    print map(len,indices_in_fjords)
 
-        #floor
-        to_index = int( (i+1) * round(float(number_of_images_total) / float(k)) )
+    # tests
+    test = []
+    for fjord in indices_in_fjords:
+        test += fjord
+    if not test == indices:
+        print 'not the same!'
 
-        if (i+1)==k:
-            to_index = number_of_images_total
+    selected = 2
+    train_indices, validation_indices = kfold(indices_in_fjords, selected)
+    print kfold(indices_in_fjords, 0)
+    print kfold(indices_in_fjords, 1)
 
-        print "from", last_index, "to", to_index
-        last_index = to_index+1
+    [y1, y2] = dataset.getDataLabels_split_only_y(validation_split=model_settings["validation_split"])
+    y = y1.tolist()+y2.tolist()
+    y = range(100,160)
+    print y
+
+    selected_data = indices_to_data(validation_indices, y)
+    print selected_data
 
     history = None
     return history
