db60a1fd728fe97a9163f2b33d4ad9b4f410a024
previtus <previtus@gmail.com>
2017-06-21 20:59:21 +0900

    Graphing

--- ResultsGraphing/1_original-expand_5556x_markable_640x640.py
+++ ResultsGraphing/1_original-expand_5556x_markable_640x640.py
@@ -6,36 +6,49 @@ path_folder += '/data/k-fold-tests/5556x_markable_640x640__ExpandDataset_5556x_m
 original_history_path = path_folder+'original____1714013.arien-pro.ics.muni.cz_ExpandDataset_5556x_markable_640x640_kfold10_originalComparison/history/5556x_markable_640x640_original_5556x_markable_640x640_500.npy'
 extended_history_path = path_folder+'expanded___1714012.arien-pro.ics.muni.cz_ExpandDataset_5556x_markable_640x640_kfold10/history/5556x_markable_640x640_2x_expanded_expanded_5556x_markable_640x640_2x_expanded_500.npy'
 
-from Downloader.VisualizeHistory import loadHistory, visualize_history, visualize_special_histories,visualize_special_history
+from Downloader.VisualizeHistory import loadHistory, visualize_history, visualize_special_histories
 from Omnipresent import len_
+from ResultsGraphing.custom import visualize_special_histories_custom, count_averages, draw_items_for_legend, onefrom, draw_normal_data, draw_titles_legends, draw_avg_data, save
 
 original_history = loadHistory(original_history_path)
 extended_history = loadHistory(extended_history_path)
 
-print original_history.keys()
-print extended_history.keys()
-
-Settings = {}
-Settings["folders"] = {}
-Settings["folders"]["together_graph_filename"] = ''
-
-
-#visualize_special_history(original_history, Settings, save=False, show=True)
-
-from ResultsGraphing.custom import visualize_special_histories_custom
-import matplotlib.pyplot as plt
-
-plt.figure()
 
 colors1 = ['grey', 'lightgreen', 'red', 'green'] # << Green is original
 colors2 = ['grey', 'lightblue', 'red', 'blue']   # << Blue  is extended
-#train_color, val_color, avg_train_color, avg_val_color
+#          train_color, val_color, avg_train_color, avg_val_color
 
-custom_title = 'original model'
-visualize_special_histories_custom(plt, colors1, original_history["all_histories_of_this_model"], plotvalues='loss', show=True, save=False, custom_title=custom_title, just_val=True)
+original_history = count_averages(original_history, 'loss')
+extended_history = count_averages(extended_history, 'loss')
 
-custom_title = 'extended model'
-visualize_special_histories_custom(plt, colors2, extended_history["all_histories_of_this_model"], plotvalues='loss', show=True, save=False, custom_title=custom_title, just_val=True)
+print original_history.keys()
+
+
+import matplotlib.pyplot as plt
+plt.figure()
+
+
+items_to_draw  = [original_history["avg_val_loss"], onefrom(original_history,"val_loss")]
+items_to_draw += [extended_history["avg_val_loss"], onefrom(extended_history,"val_loss")]
+
+names_to_print  = ["original average val", "original val"]
+names_to_print += ["extended average val", "extended val"]
+colors_to_use  = [colors1[3], colors1[1], colors2[3], colors2[1]]
+linestyles = ['solid', 'dashed', 'solid', 'dashed']
+
+leg = []
+[plt, leg] = draw_items_for_legend(plt, leg, items_to_draw, names_to_print, colors_to_use, linestyles)
+
+plt = draw_normal_data("val_loss", original_history["all_histories_of_this_model"], colors1[1], 'dashed', plt)
+plt = draw_normal_data("val_loss", extended_history["all_histories_of_this_model"], colors2[1], 'dashed', plt)
+
+plt = draw_avg_data(original_history["avg_val_loss"], colors1[3], 'solid', plt)
+plt = draw_avg_data(extended_history["avg_val_loss"], colors2[3], 'solid', plt)
+
+custom_title = 'original vs extended model'
+draw_titles_legends(plt, leg, custom_title)
+
+save(plt,True,'graphs/1_original-expand_5556x_markable_640x640/automatic')
 
 plt.show()
 plt.clf()
--- ResultsGraphing/custom.py
+++ ResultsGraphing/custom.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-def visualize_special_histories_custom(plt, colors, histories, plotvalues='loss', show=True, save=False, save_path='', custom_title=None, just_val=False):
+def onefrom(ar, plotvalues):
+    return ar["all_histories_of_this_model"][0][plotvalues]
 
-    train_color = colors[0]
-    val_color = colors[1]
-
-    avg_train_color = colors[2]
-    avg_val_color = colors[3]
+def count_averages(special_history, plotvalues):
+    histories = special_history["all_histories_of_this_model"]
 
     avg_train = []
     avg_val = []
@@ -25,26 +23,70 @@ def visualize_special_histories_custom(plt, colors, histories, plotvalues='loss'
         avg_train.append( np.mean(trains) )
         avg_val.append( np.mean(vals) )
 
+    special_history['avg_'+plotvalues] = avg_train
+    special_history['avg_val_'+plotvalues] = avg_val
 
-    if custom_title is None:
-        custom_title = 'model ' + plotvalues
-    if just_val:
-        custom_title = custom_title + ' (just validation results)'
+    return special_history
+
+def draw_normal_data(plotvalues, items, color, linestyle, plt):
+    for item in items:
+        plt.plot(item[plotvalues], linestyle=linestyle, color=color)
+
+    return plt
+
+def draw_avg_data(item, color, linestyle, plt):
+    plt.plot(item, linestyle=linestyle, color=color)
+
+    return plt
+
+def draw_items_for_legend(plt, leg, items_to_draw, names_to_print, colors_to_use, linestyles):
+    for i in range(0, len(items_to_draw)):
+        item = items_to_draw[i][0]
+
+        color = colors_to_use[i]
+        linestyle = linestyles[i]
+        name = names_to_print[i]
+
+        plt.plot(item, linestyle=linestyle, color=color)
+        leg.append(name)
+    return [plt, leg]
+
+def draw_titles_legends(plt, leg, custom_title):
+    plt.title(custom_title)
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+
+    #plt.legend(leg, loc='lower left')
+    plt.legend(leg, loc='best')
+
+    return plt
+
+def save(plt, save, path):
+    if save:
+        plt.savefig(path)
+        plt.savefig(path+'.pdf', format='pdf')
+
+    return plt
+
+def visualize_special_histories_custom(plt, leg, colors, special_history, plotvalues='loss', show=True, save=False, save_path='', custom_title=None, just_val=False):
+    histories = special_history["all_histories_of_this_model"]
+    avg_train = special_history['avg_'+plotvalues]
+    avg_val = special_history['avg_val_'+plotvalues]
+
+    train_color = colors[0]
+    val_color = colors[1]
+    avg_train_color = colors[2]
+    avg_val_color = colors[3]
+
+
 
     i = 0
-    leg = []
-    if not just_val:
-        leg.append('average training')
-    leg.append('average validation')
 
-    if not just_val:
-        leg.append('training errors')
-    leg.append('validation errors')
 
     # now averages:
     if not just_val:
-        plt.plot(avg_train, color=avg_train_color)
-    plt.plot(avg_val, color=avg_val_color)
+        plt.plot(avg_train, linestyle='solid', color=avg_train_color)
+    plt.plot(avg_val, linestyle='solid', color=avg_val_color)
 
     for hi in histories:
         # list all data in history
@@ -61,13 +103,6 @@ def visualize_special_histories_custom(plt, colors, histories, plotvalues='loss'
     plt.plot(avg_val, color=avg_val_color)
 
 
-    plt.title(custom_title)
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-
-
-    #plt.legend(leg, loc='lower left')
-    plt.legend(leg, loc='best')
     if save:
         plt.savefig(save_path) #+plotvalues+'.png')
         plt.savefig(save_path+'.pdf', format='pdf')
