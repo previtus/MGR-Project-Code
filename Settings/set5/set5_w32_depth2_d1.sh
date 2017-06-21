d9df9593828b30112eacaad414516a1cceb58c5b
previtus <previtus@gmail.com>
2017-06-22 00:06:37 +0900

    set5 - 4x4 osm with various width - depth setting

---
+++ Settings/set5/set5_w32_depth2_d1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH="/storage/brno2/home/previtus/anaconda2/bin:$PATH"
+cd /auto/brno2/home/previtus/MGR-Project-Code/
+
+python run_on_server.py Settings/set5/set5_w32_depth2_d1.py $PBS_JOBID
