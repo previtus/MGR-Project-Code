fc67391f82f14d44d7b63c940d50d1dfe8116442
previtus <previtus@gmail.com>
2017-06-22 22:10:26 +0900

    wooops

--- Settings/set6/var_cnn_test_proper_dataset_xception.py
+++ Settings/set6/var_cnn_test_proper_dataset_xception.py
@@ -6,18 +6,16 @@ def Setup(Settings,DefaultModel):
     Settings["graph_histories"] = ['together'] #['all','together',[],[1,0],[0,0,0],[]]
 
     n=0
-
-    Settings["models"][n]["dataset_pointer"] = 0 # 0 - reuse the first dataset
+    Settings["models"][n]["dataset_pointer"] = -1 # 0 - reuse the first dataset
     Settings["models"][n]["dataset_name"] = "5556x_markable_640x640"
     Settings["models"][n]["pixels"] = 640
     Settings["models"][n]["cnn_model"] = 'xception'
     Settings["models"][n]["unique_id"] = 'xception_cnn'
     Settings["models"][n]["cooking_method"] = 'generators' # 'direct' or 'generators'
-    Settings["models"][n]["epochs"] = 5
     Settings["models"][n]["dump_file_override"] = 'SegmentsData_marked_R100_4Tables.dump'
     Settings["models"][n]["model_type"] = 'img_osm_mix'
 
-    Settings["models"][n]["epochs"] = 1000
+    Settings["models"][n]["epochs"] = 500
 
     Settings["models"][n]["k_fold_crossvalidation"] = True
     Settings["models"][n]["crossvalidation_k"] = 10
