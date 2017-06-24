7248cc48cc15e0ad0e839c8a42e0c2b244b4c2e5
previtus <previtus@gmail.com>
2017-06-25 00:05:02 +0900

    20 epochs version too, what if its just tooo slow

---
+++ Settings/set6b/var_cnn_test_proper_dataset_vgg16_10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH="/storage/brno2/home/previtus/anaconda2/bin:$PATH"
+cd /auto/brno2/home/previtus/MGR-Project-Code/
+
+python run_on_server.py Settings/set6b/var_cnn_test_proper_dataset_vgg16_10.py $PBS_JOBID
