8f8d37177a546e1b5be249c5331adfc54eeec8f4
previtus <previtus@gmail.com>
2017-06-25 00:08:59 +0900

    oops

--- Settings/set6b/run6b10s.sh
+++ Settings/set6b/run6b10s.sh
@@ -3,10 +3,10 @@
 # d1
 
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg16_100.sh -l mem=64gb -l ncpus=8
+qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg16_10.sh -l mem=64gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg19_100.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg19_10.sh -l mem=128gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_inception_v3_100.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_inception_v3_10.sh -l mem=128gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_xception_100.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_xception_10.sh -l mem=128gb -l ncpus=8
