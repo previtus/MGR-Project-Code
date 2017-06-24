5730f4bf5f0654aaacc6e1a054038c26a772ef85
previtus <previtus@gmail.com>
2017-06-25 00:10:30 +0900

    reorder

--- Settings/set6/run6.sh
+++ Settings/set6/run6.sh
@@ -6,6 +6,3 @@ qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_inception_v3.sh -l mem=12
 
 qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_xception.sh -l mem=128gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_inception_v3_100.sh -l mem=128gb -l ncpus=8
-
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_xception_100.sh -l mem=128gb -l ncpus=8
--- Settings/set6/run6_vggs.sh
+++ Settings/set6/run6_vggs.sh
@@ -6,7 +6,3 @@ qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg16.sh -l mem=64gb -l n
 
 qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg19.sh -l mem=128gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg16_100.sh -l mem=64gb -l ncpus=8
-
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg19_100.sh -l mem=128gb -l ncpus=8
-
