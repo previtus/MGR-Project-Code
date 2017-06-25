89bd9cd36686c22bf79987df3980184ce47b72d7
previtus <previtus@gmail.com>
2017-06-25 15:31:16 +0900

    for graphs

---
+++ Settings/set6b/var_cnn_test_proper_dataset_resnet50_100.py
@@ -0,0 +1,25 @@
+def Setup(Settings, DefaultModel):
+    # set6/var_cnn_test_proper_dataset_resnet50_100.py
+
+    Settings["experiment_name"] = "var_cnn_test_proper_dataset_resnet50_kfold_20"
+
+    Settings["graph_histories"] = ['together']  # ['all','together',[],[1,0],[0,0,0],[]]
+
+    n = 0
+    Settings["models"][n]["dataset_pointer"] = -1  # 0 - reuse the first dataset
+    Settings["models"][n]["dataset_name"] = "5556x_markable_640x640"
+    Settings["models"][n]["pixels"] = 640
+    Settings["models"][n]["cnn_model"] = 'resnet50'
+    Settings["models"][n]["unique_id"] = 'resnet50_cnn'
+    Settings["models"][n]["cooking_method"] = 'generators'  # 'direct' or 'generators'
+    Settings["models"][n]["dump_file_override"] = 'SegmentsData_marked_R100_4Tables.dump'
+    Settings["models"][n]["model_type"] = 'img_osm_mix'
+
+    Settings["models"][n]["epochs"] = 100
+
+    Settings["models"][n]["k_fold_crossvalidation"] = True
+    Settings["models"][n]["crossvalidation_k"] = 10
+
+    Settings["graph_histories"] = []
+
+    return Settings
