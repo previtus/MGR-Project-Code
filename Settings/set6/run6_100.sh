2523ac6b4a7146ae046031f7246926c1b0f50280
previtus <previtus@gmail.com>
2017-06-25 15:37:07 +0900

    commands are reasonable for 47hrs, 2w queue wont ever happen

--- Settings/set6/run6_100.sh
+++ Settings/set6/run6_100.sh
@@ -2,11 +2,11 @@
 
 # d1
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_inception_v3_100.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=47:30:00 var_cnn_test_proper_dataset_inception_v3_100.sh -l mem=128gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_xception_100.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=47:30:00 var_cnn_test_proper_dataset_xception_100.sh -l mem=128gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg16_100.sh -l mem=64gb -l ncpus=8
+qsub -l walltime=47:30:00 var_cnn_test_proper_dataset_vgg16_100.sh -l mem=64gb -l ncpus=8
 
-qsub -l walltime=167:30:00 var_cnn_test_proper_dataset_vgg19_100.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=47:30:00 var_cnn_test_proper_dataset_vgg19_100.sh -l mem=128gb -l ncpus=8
 
