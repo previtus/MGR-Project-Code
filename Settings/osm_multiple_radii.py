cd2eea2b37e2e41402afb3ad77219e85f8434ea2
previtus <previtus@gmail.com>
2017-06-27 20:50:51 +0900

    proper test

--- Settings/osm_multiple_radii.py
+++ Settings/osm_multiple_radii.py
@@ -9,12 +9,21 @@ def Setup(Settings,DefaultModel):
     #Settings["models"][n]["dataset_name"] = "5556x_reslen20_299px" # "1200x_markable_299x299", "5556x_mark_res_299x299", "5556x_markable_640x640"
     Settings["models"][n]["dataset_name"] = "5556x_markable_640x640"
     Settings["models"][n]["model_type"] = 'osm_only'
-    Settings["models"][n]["unique_id"] = 'normal_osm_only'
+    Settings["models"][n]["unique_id"] = 'multiple_radii'
     Settings["models"][n]["top_repeat_FC_block"] = 2
     Settings["models"][n]["epochs"] = 50
-    Settings["models"][n]["edit_osm_vec"] = ''
     Settings["models"][n]["dump_file_override"] = 'SegmentsData_marked_R100_4Tables.dump'
 
     Settings["models"][n]["special_case"] = 'OSM_Multiple_Radii'
+
+    Settings["models"].append(DefaultModel.copy())
+    n+=1
+    Settings["models"][n]["dataset_pointer"] = -1
+    Settings["models"][n]["dataset_name"] = "5556x_markable_640x640"
+    Settings["models"][n]["model_type"] = 'osm_only'
+    Settings["models"][n]["unique_id"] = 'only_r100'
+    Settings["models"][n]["top_repeat_FC_block"] = 2
+    Settings["models"][n]["epochs"] = 100
+    Settings["models"][n]["dump_file_override"] = 'SegmentsData_marked_R100_4Tables.dump'
 
     return Settings
