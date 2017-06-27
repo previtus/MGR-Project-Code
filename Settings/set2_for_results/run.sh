e28c1cf71919823ae743c355b4377a9798f778ac
previtus <previtus@gmail.com>
2017-06-27 18:07:21 +0900

    name

--- Settings/set2_for_results/run.sh
+++ Settings/set2_for_results/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qsub -l walltime=23:30:00 var_cnn_test_proper_dataset_299.sh -l mem=128gb -l ncpus=8
+qsub -l walltime=23:30:00 var_cnn_test_proper_dataset_299x300ep.sh -l mem=128gb -l ncpus=8
