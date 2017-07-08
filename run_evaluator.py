64aaf5bc98a749e5261b9f213f4ea584d38224e3
previtus <previtus@gmail.com>
2017-07-08 20:07:01 +0900

    new run

--- run_evaluator.py
+++ run_evaluator.py
@@ -24,13 +24,17 @@ OSM_settings_file = 'Settings/set3/type_of_model_osm_kfold_d3.py'
 OSM_model_file = folder_path + 'SelectedModels/1771032.arien-pro.ics.muni.cz_set5_w64_depth2_d1/models/osm_resnet50.h5_osmtop.h5'
 OSM_settings_file = 'Settings/set5/set5_w64_depth2_d1.py'
 
+# MIX Flagship Model !
+MIX_model_file = folder_path + 'SelectedModels/1788956.arien-pro.ics.muni.cz_builds_aggresive_expand_dataset_normalmarkable_kfold/models/expanded: 5556x_markable_640x640_2x_agressive_expanded_resnet50.h5_top.h5'
+MIX_settings_file = 'Settings/set7missing/builds_aggresive_expand_dataset_normalmarkable_kfold.py'
+
 
 if len(sys.argv) > 2:
     model_file = (sys.argv[1])  # var1
     settings_file = (sys.argv[2])  # var2
 
-img_mse, img_mae = evaluator(IMG_model_file, IMG_settings_file, 'marked_from_img_1769353.geojson')
-mix_mse, mix_mae = evaluator(MIX_model_file, MIX_settings_file, 'marked_from_mix_1761335.geojson')
+#img_mse, img_mae = evaluator(IMG_model_file, IMG_settings_file, 'marked_from_img_1769353.geojson')
+mix_mse, mix_mae = evaluator(MIX_model_file, MIX_settings_file, 'marked_from_mix_flag1788956.geojson')
 #osm_mse, osm_mae = evaluator(OSM_model_file, OSM_settings_file)
 
 '''
