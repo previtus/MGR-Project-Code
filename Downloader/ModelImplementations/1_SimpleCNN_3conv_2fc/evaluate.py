80655b26942b40751ea3acdd3a33ef7978251566
previtus <previtus@gmail.com>
2017-07-08 21:30:42 +0900

    CLEANUP useless files

--- Downloader/ModelImplementations/1_SimpleCNN_3conv_2fc/evaluate.py
+++
@@ -1,8 +0,0 @@
-from keras.models import load_model
-from Tester import TestModel
-
-PathToImages = '../../1100downloaded_vII/'
-LocalFolder = ''
-
-model = load_model(LocalFolder+'data/pokusCNN_V2_s1000_e5.h5')
-TestModel(model, LocalFolder, PathToImages, output_model_img=True, output_history_plot=True)
--- Downloader/ModelImplementations/2_VGG16_no_finetuning/readme.txt
+++
@@ -1,7 +0,0 @@
-2_VGG16_no_finetuning
-
-Model:
-    - Uses parts of VGG16 pretrained on ImageNet
-
-Input Dataset:
-    - set of 2000 train and 800 validation generated from the original set of 1385 Segments (via Prepare_DataLabels_withGeneratedData)
