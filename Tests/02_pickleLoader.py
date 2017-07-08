1d9d4af4e4b02a4c64ba21dfd6194f8d2998bd26
previtus <previtus@gmail.com>
2017-07-08 20:45:03 +0900

    Test are no longer needed. They were used to test small isolated code segments.

--- Tests/02_pickleLoader.py
+++
@@ -1,8 +0,0 @@
-from GenerateGIFAnimation import *
-import pickle
-import os
-
-with open('../GraphParser/test_out.dump') as f:
-    Segments = pickle.load(f)
-    os.chdir('../GraphParser')
-    GenerateGIFAnimation(Segments, 'test.gif')
--- Tests/ResNet152_tests/run_resnet152.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qsub -l walltime=95:30:00 task_resnet152.sh -l mem=32gb -l ncpus=8
