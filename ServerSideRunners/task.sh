9895890b5d43066d81852030d8859e99452b9da6
previtus <previtus@gmail.com>
2017-07-22 14:20:59 +0900

    cleanup of proper name for Setting file

--- ServerSideRunners/task.sh
+++ ServerSideRunners/task.sh
@@ -3,5 +3,5 @@
 export PATH="/storage/brno2/home/previtus/anaconda2/bin:$PATH"
 cd /auto/brno2/home/previtus/MGR-Project-Code/
 
-python run_on_server.py Settings/top_number_of_fc_blocks.py $PBS_JOBID
+python run_on_server.py Settings/minimal_model.py $PBS_JOBID
 
